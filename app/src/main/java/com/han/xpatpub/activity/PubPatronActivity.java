b05d6bae575b5989231441e8c1e4156df332aa35
Kristijan Drača <kristijandraca@gmail.com>
2015-02-04 14:25:01 +0100

    prepare for braintree payment

--- app/src/main/java/com/han/xpatpub/activity/PubPatronActivity.java
+++ app/src/main/java/com/han/xpatpub/activity/PubPatronActivity.java
@@ -11,13 +11,18 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.braintreepayments.api.dropin.BraintreePaymentActivity;
 import com.han.xpatpub.R;
 import com.han.xpatpub.asynctasks.UserAsyncTask;
 import com.han.xpatpub.model.Action;
 import com.han.xpatpub.model.GlobalData;
 import com.han.xpatpub.utility.DialogUtility;
 import com.han.xpatpub.utility.MessagingUtility;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
 
+import org.apache.http.Header;
 
 
 public class PubPatronActivity extends AbstractedActivity {
@@ -138,7 +143,7 @@ public class PubPatronActivity extends AbstractedActivity {
 			
 			@Override
 			public void onClick(View v) {					
-				if (dialog == null || !dialog.isShowing()) {
+				/*if (dialog == null || !dialog.isShowing()) {
 			    	dialog = new AlertDialog.Builder(PubPatronActivity.this)
 						.setTitle("Send Request to Upgrade Account")
 						.setMessage("We are accepting pub owners by request only during our Beta launch. Please click send and we will have someone contact you via email to setup your Pub Owner account!")
@@ -156,7 +161,10 @@ public class PubPatronActivity extends AbstractedActivity {
 							public void onClick(DialogInterface dialog, int which) {}
 							
 						}).show();
-		    	}
+		    	}*/
+                Intent intent = new Intent(PubPatronActivity.this, BraintreePaymentActivity.class);
+                intent.putExtra(BraintreePaymentActivity.EXTRA_CLIENT_TOKEN, "eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiI5ZWE3ZDdjMjE5ODIwNmI2NDdiNWExZWI4MGMxMDVlMjQ1NzhiNWZkNmU1NWY5ODU4OWVkYmNmNmYzMTY5Nzk3fGNyZWF0ZWRfYXQ9MjAxNS0wMi0wNFQxMjoyMDoxMC45ODY5NjUyNDgrMDAwMFx1MDAyNm1lcmNoYW50X2lkPWRjcHNweTJicndkanIzcW5cdTAwMjZwdWJsaWNfa2V5PTl3d3J6cWszdnIzdDRuYzgiLCJjb25maWdVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvZGNwc3B5MmJyd2RqcjNxbi9jbGllbnRfYXBpL3YxL2NvbmZpZ3VyYXRpb24iLCJjaGFsbGVuZ2VzIjpbXSwicGF5bWVudEFwcHMiOltdLCJjbGllbnRBcGlVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvZGNwc3B5MmJyd2RqcjNxbi9jbGllbnRfYXBpIiwiYXNzZXRzVXJsIjoiaHR0cHM6Ly9hc3NldHMuYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhdXRoVXJsIjoiaHR0cHM6Ly9hdXRoLnZlbm1vLnNhbmRib3guYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhbmFseXRpY3MiOnsidXJsIjoiaHR0cHM6Ly9jbGllbnQtYW5hbHl0aWNzLnNhbmRib3guYnJhaW50cmVlZ2F0ZXdheS5jb20ifSwidGhyZWVEU2VjdXJlRW5hYmxlZCI6dHJ1ZSwidGhyZWVEU2VjdXJlIjp7Imxvb2t1cFVybCI6Imh0dHBzOi8vYXBpLnNhbmRib3guYnJhaW50cmVlZ2F0ZXdheS5jb206NDQzL21lcmNoYW50cy9kY3BzcHkyYnJ3ZGpyM3FuL3RocmVlX2Rfc2VjdXJlL2xvb2t1cCJ9LCJwYXlwYWxFbmFibGVkIjp0cnVlLCJwYXlwYWwiOnsiZGlzcGxheU5hbWUiOiJBY21lIFdpZGdldHMsIEx0ZC4gKFNhbmRib3gpIiwiY2xpZW50SWQiOm51bGwsInByaXZhY3lVcmwiOiJodHRwOi8vZXhhbXBsZS5jb20vcHAiLCJ1c2VyQWdyZWVtZW50VXJsIjoiaHR0cDovL2V4YW1wbGUuY29tL3RvcyIsImJhc2VVcmwiOiJodHRwczovL2Fzc2V0cy5icmFpbnRyZWVnYXRld2F5LmNvbSIsImFzc2V0c1VybCI6Imh0dHBzOi8vY2hlY2tvdXQucGF5cGFsLmNvbSIsImRpcmVjdEJhc2VVcmwiOm51bGwsImFsbG93SHR0cCI6dHJ1ZSwiZW52aXJvbm1lbnROb05ldHdvcmsiOnRydWUsImVudmlyb25tZW50Ijoib2ZmbGluZSIsInVudmV0dGVkTWVyY2hhbnQiOmZhbHNlLCJtZXJjaGFudEFjY291bnRJZCI6InN0Y2gybmZkZndzenl0dzUiLCJjdXJyZW5jeUlzb0NvZGUiOiJVU0QifSwiY29pbmJhc2VFbmFibGVkIjpmYWxzZSwibWVyY2hhbnRJZCI6ImRjcHNweTJicndkanIzcW4iLCJ2ZW5tbyI6Im9mZmxpbmUiLCJhcHBsZVBheSI6eyJzdGF0dXMiOiJtb2NrIiwiY291bnRyeUNvZGUiOiJVUyIsImN1cnJlbmN5Q29kZSI6IlVTRCIsIm1lcmNoYW50SWRlbnRpZmllciI6Im1lcmNoYW50LmNvbS5icmFpbnRyZWVwYXltZW50cy5kZXYtZGNvcGVsYW5kIiwic3VwcG9ydGVkTmV0d29ya3MiOlsidmlzYSIsIm1hc3RlcmNhcmQiLCJhbWV4Il19fQ==");
+                startActivityForResult(intent, REQUEST_CODE);
 			}
 		});
 		
@@ -189,8 +197,37 @@ public class PubPatronActivity extends AbstractedActivity {
 				new UserAsyncTask(PubPatronActivity.this).execute(Action.ACTION_UPDATE_USER_PRIVACY, "3");
 			}
         });
-	}	
-	
+	}
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == BraintreePaymentActivity.RESULT_OK) {
+                String paymentMethodNonce = data.getStringExtra(BraintreePaymentActivity.EXTRA_PAYMENT_METHOD_NONCE);
+                postNonceToServer(paymentMethodNonce);
+            }
+        }
+    }
+
+    void postNonceToServer(String nonce) {
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("nonce", nonce);
+        client.post("https://your-server/payment-method-nonce", params,
+                new AsyncHttpResponseHandler() {
+                    // TODO implementation here
+                    @Override
+                    public void onSuccess(int i, Header[] headers, byte[] bytes) {
+
+                    }
+
+                    @Override
+                    public void onFailure(int i, Header[] headers, byte[] bytes, Throwable throwable) {
+
+                    }
+
+                }
+        );
+    }
 //	@Override
 //	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 //		if (requestCode == REQUEST_CODE) {
